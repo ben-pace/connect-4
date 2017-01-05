b88eb408f39e343964bbb436e8d4bab1f3dfcabc
ben-pace <benitopace@gmail.com>
2017-01-05 12:10:41 +0100

    Update connect_4.py

--- connect_4.py
+++ connect_4.py
@@ -30,20 +30,23 @@ def make_move(player, current_board):
 
 	if open == -1:
 		print("That column is full! Pick another column.")
-		return make_move(player, current_board)
+		make_move(player, current_board)
 	else:
-		board[open][move] = colour
-		if win_condition(board):
+		current_board[open][move] = colour
+		if win_condition(current_board):
+			display_board(current_board)
 			print("Player %s has won! Congratulations!" % (player + 1))
 		else:
-			return make_move(1 - player, current_board)
+			make_move(1 - player, current_board)
 
 # Finds the row (counting up from the bottom) that is open
 # and returns -1 if it's full
 def check_column(column):
 	open = 5
-	while board[5 - open][column] != "O":
+	while board[open][column] != "O":
 		open -= 1
+		if open == -1:
+			break
 	return open
 
 # Takes in a board and checks if the game is over
@@ -66,11 +69,20 @@ def check_win_here(board, row, col):
 	tile = board[row][col]
 	for direction in directions:
 		for y in range(3):
-			if row + y*direction[0] >= 0 and row + y*direction[0] <= 5 and col + y*direction[1] >= 0 and col + y*direction[1] <= 6:
+			# Checks to make sure you're in range
+			if (row + (y+1)*direction[0] >= 0 and 
+				row + (y+1)*direction[0] <= 5 and 
+				col + (y+1)*direction[1] >= 0 and 
+				col + (y+1)*direction[1] <= 6):
+				# Checks if the tile is the wrong colour
 				if board[row + y*direction[0]][col + y*direction[1]] != tile:
+					win = False
 					break
+				if y == 3:
+					win = True
 			else:
-				win = True
+				win = False
+				break
 		if win:
 			break
 	return win
